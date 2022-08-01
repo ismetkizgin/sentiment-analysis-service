e7d9bee92d35322143afbf4fc248e71a58e90855
ismet kizgin <info@ismetkizgin.com.tr>
2022-08-01 10:42:36 +0300

    feat: dataset data visualization

--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
 
 column = ['comment']
 df = pd.read_csv('dataset.csv', encoding ='utf8', sep=',')
@@ -21,7 +22,9 @@ pickle.dump(vect, open('vect.pkl', 'wb'))
 X_vectorized = vect.transform(X) 
 
 from sklearn.linear_model import LogisticRegression
-model = LogisticRegression()
+model = LogisticRegression(max_iter=1000)
 model.fit(X_vectorized, y)
-
 pickle.dump(model, open('model.pkl','wb'))
+
+df['positivity'].value_counts().plot.bar()
+plt.savefig('graph.png')
